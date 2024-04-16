9ed82bde8cbe93f6bde01653a33ec4cb106e12e5
Shivam <gautamshivam043@gmail.com>
2024-04-17 01:52:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+// import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import container from "react-bootstrap/Container";
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <container>shivam</container>
+        </>
+    );
+}
+
+export default App;
