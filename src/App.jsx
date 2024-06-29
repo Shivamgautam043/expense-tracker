bcf10b971aeed91d6ca0401119ba5374ec16b3e9
Shivam <gautamshivam043@gmail.com>
2024-06-30 03:14:12 +0530

    added new  section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
+import {Button, Stack} from "react-bootstrap"
 // import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
-import container from "react-bootstrap/Container";
+import Container from "react-bootstrap/Container";
 function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <>
-            <container>shivam</container>
-        </>
+        <Container>
+          <Stack direction="horizontal" gap="2" className="mb-4">
+            <h1 className="me-auto">Budget</h1>
+            <Button variant="primary">Add Budget</Button>
+            <Button variant="outline-primary">Add Expense</Button>
+          </Stack>
+
+          <div style={{display:"grid", gridTemplateColumns:"repeat(auto-fill,minmax(300px,1fr))",gap:"1rem", alignItem:"flex-start"}}></div>
+        </Container>
     );
 }
 
